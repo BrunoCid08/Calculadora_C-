d43fdc60aa950faa77eb68815aab5194bed2535e
BrunoCid08 <83646159+BrunoCid08@users.noreply.github.com>
2021-08-25 04:53:56 -0700

    Add files via upload

---
+++ Calculadora CSharp/Calculadora CSharp/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace Calculadora_CSharp
+{
+    partial class Interface
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Interface));
+            this.txt_resultado = new System.Windows.Forms.TextBox();
+            this.lbl_operacao = new System.Windows.Forms.Label();
+            this.btn_c = new System.Windows.Forms.Button();
+            this.btn_div = new System.Windows.Forms.Button();
+            this.btn_vezes = new System.Windows.Forms.Button();
+            this.btn_9 = new System.Windows.Forms.Button();
+            this.btn_8 = new System.Windows.Forms.Button();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.btn_mais = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_menos = new System.Windows.Forms.Button();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_igual = new System.Windows.Forms.Button();
+            this.btn_virgula = new System.Windows.Forms.Button();
+            this.btn_0 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_resultado
+            // 
+            resources.ApplyResources(this.txt_resultado, "txt_resultado");
+            this.txt_resultado.Name = "txt_resultado";
+            this.txt_resultado.ReadOnly = true;
+            // 
+            // lbl_operacao
+            // 
+            resources.ApplyResources(this.lbl_operacao, "lbl_operacao");
+            this.lbl_operacao.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_operacao.Name = "lbl_operacao";
+            // 
+            // btn_c
+            // 
+            resources.ApplyResources(this.btn_c, "btn_c");
+            this.btn_c.Name = "btn_c";
+            this.btn_c.UseVisualStyleBackColor = true;
+            this.btn_c.Click += new System.EventHandler(this.btn_c_Click);
+            // 
+            // btn_div
+            // 
+            resources.ApplyResources(this.btn_div, "btn_div");
+            this.btn_div.Name = "btn_div";
+            this.btn_div.UseVisualStyleBackColor = true;
+            this.btn_div.Click += new System.EventHandler(this.btn_div_Click);
+            // 
+            // btn_vezes
+            // 
+            resources.ApplyResources(this.btn_vezes, "btn_vezes");
+            this.btn_vezes.Name = "btn_vezes";
+            this.btn_vezes.UseVisualStyleBackColor = true;
+            this.btn_vezes.Click += new System.EventHandler(this.btn_vezes_Click);
+            // 
+            // btn_9
+            // 
+            resources.ApplyResources(this.btn_9, "btn_9");
+            this.btn_9.Name = "btn_9";
+            this.btn_9.UseVisualStyleBackColor = true;
+            this.btn_9.Click += new System.EventHandler(this.btn_9_Click);
+            // 
+            // btn_8
+            // 
+            resources.ApplyResources(this.btn_8, "btn_8");
+            this.btn_8.Name = "btn_8";
+            this.btn_8.UseVisualStyleBackColor = true;
+            this.btn_8.Click += new System.EventHandler(this.btn_8_Click);
+            // 
+            // btn_7
+            // 
+            resources.ApplyResources(this.btn_7, "btn_7");
+            this.btn_7.Name = "btn_7";
+            this.btn_7.UseVisualStyleBackColor = true;
+            this.btn_7.Click += new System.EventHandler(this.btn_7_Click);
+            // 
+            // btn_mais
+            // 
+            resources.ApplyResources(this.btn_mais, "btn_mais");
+            this.btn_mais.Name = "btn_mais";
+            this.btn_mais.UseVisualStyleBackColor = true;
+            this.btn_mais.Click += new System.EventHandler(this.btn_mais_Click);
+            // 
+            // btn_3
+            // 
+            resources.ApplyResources(this.btn_3, "btn_3");
+            this.btn_3.Name = "btn_3";
+            this.btn_3.UseVisualStyleBackColor = true;
+            this.btn_3.Click += new System.EventHandler(this.btn_3_Click);
+            // 
+            // btn_2
+            // 
+            resources.ApplyResources(this.btn_2, "btn_2");
+            this.btn_2.Name = "btn_2";
+            this.btn_2.UseVisualStyleBackColor = true;
+            this.btn_2.Click += new System.EventHandler(this.btn_2_Click);
+            // 
+            // btn_1
+            // 
+            resources.ApplyResources(this.btn_1, "btn_1");
+            this.btn_1.Name = "btn_1";
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // btn_menos
+            // 
+            resources.ApplyResources(this.btn_menos, "btn_menos");
+            this.btn_menos.Name = "btn_menos";
+            this.btn_menos.UseVisualStyleBackColor = true;
+            this.btn_menos.Click += new System.EventHandler(this.btn_menos_Click);
+            // 
+            // btn_6
+            // 
+            resources.ApplyResources(this.btn_6, "btn_6");
+            this.btn_6.Name = "btn_6";
+            this.btn_6.UseVisualStyleBackColor = true;
+            this.btn_6.Click += new System.EventHandler(this.btn_6_Click);
+            // 
+            // btn_5
+            // 
+            resources.ApplyResources(this.btn_5, "btn_5");
+            this.btn_5.Name = "btn_5";
+            this.btn_5.UseVisualStyleBackColor = true;
+            this.btn_5.Click += new System.EventHandler(this.btn_5_Click);
+            // 
+            // btn_4
+            // 
+            resources.ApplyResources(this.btn_4, "btn_4");
+            this.btn_4.Name = "btn_4";
+            this.btn_4.UseVisualStyleBackColor = true;
+            this.btn_4.Click += new System.EventHandler(this.btn_4_Click);
+            // 
+            // btn_igual
+            // 
+            resources.ApplyResources(this.btn_igual, "btn_igual");
+            this.btn_igual.Name = "btn_igual";
+            this.btn_igual.UseVisualStyleBackColor = true;
+            this.btn_igual.Click += new System.EventHandler(this.btn_igual_Click);
+            // 
+            // btn_virgula
+            // 
+            resources.ApplyResources(this.btn_virgula, "btn_virgula");
+            this.btn_virgula.Name = "btn_virgula";
+            this.btn_virgula.UseVisualStyleBackColor = true;
+            this.btn_virgula.Click += new System.EventHandler(this.btn_virgula_Click);
+            // 
+            // btn_0
+            // 
+            resources.ApplyResources(this.btn_0, "btn_0");
+            this.btn_0.Name = "btn_0";
+            this.btn_0.UseVisualStyleBackColor = true;
+            this.btn_0.Click += new System.EventHandler(this.btn_0_Click);
+            // 
+            // Interface
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.btn_igual);
+            this.Controls.Add(this.btn_virgula);
+            this.Controls.Add(this.btn_0);
+            this.Controls.Add(this.btn_mais);
+            this.Controls.Add(this.btn_3);
+            this.Controls.Add(this.btn_2);
+            this.Controls.Add(this.btn_1);
+            this.Controls.Add(this.btn_menos);
+            this.Controls.Add(this.btn_6);
+            this.Controls.Add(this.btn_5);
+            this.Controls.Add(this.btn_4);
+            this.Controls.Add(this.btn_vezes);
+            this.Controls.Add(this.btn_9);
+            this.Controls.Add(this.btn_8);
+            this.Controls.Add(this.btn_7);
+            this.Controls.Add(this.btn_div);
+            this.Controls.Add(this.btn_c);
+            this.Controls.Add(this.lbl_operacao);
+            this.Controls.Add(this.txt_resultado);
+            this.MaximizeBox = false;
+            this.Name = "Interface";
+            this.ShowIcon = false;
+            this.Load += new System.EventHandler(this.Interface_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_resultado;
+        private System.Windows.Forms.Label lbl_operacao;
+        private System.Windows.Forms.Button btn_c;
+        private System.Windows.Forms.Button btn_div;
+        private System.Windows.Forms.Button btn_vezes;
+        private System.Windows.Forms.Button btn_9;
+        private System.Windows.Forms.Button btn_8;
+        private System.Windows.Forms.Button btn_7;
+        private System.Windows.Forms.Button btn_mais;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button btn_menos;
+        private System.Windows.Forms.Button btn_6;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_igual;
+        private System.Windows.Forms.Button btn_virgula;
+        private System.Windows.Forms.Button btn_0;
+    }
+}
+
---
+++ Calculadora CSharp/Calculadora CSharp/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_CSharp
+{
+    public partial class Interface : Form
+    {
+        decimal valor1 = 0, valor2 = 0;
+        string operacao = "";
+        public Interface()
+        {
+            InitializeComponent();
+        }
+
+        private void Interface_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_0_Click(object sender, EventArgs e)
+        {
+            txt_resultado.Text += "0";
+        }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+            txt_resultado.Text += "1";
+        }
+
+        private void btn_2_Click(object sender, EventArgs e)
+        {
+            txt_resultado.Text += "2";
+        }
+
+        private void btn_3_Click(object sender, EventArgs e)
+        {
+            txt_resultado.Text += "3";
+        }
+
+        private void btn_4_Click(object sender, EventArgs e)
+        {
+            txt_resultado.Text += "4";
+        }
+
+        private void btn_5_Click(object sender, EventArgs e)
+        {
+            txt_resultado.Text += "5";
+        }
+
+        private void btn_6_Click(object sender, EventArgs e)
+        {
+            txt_resultado.Text += "6";
+        }
+
+        private void btn_7_Click(object sender, EventArgs e)
+        {
+            txt_resultado.Text += "7";
+        }
+
+        private void btn_8_Click(object sender, EventArgs e)
+        {
+            txt_resultado.Text += "8";
+        }
+
+        private void btn_9_Click(object sender, EventArgs e)
+        {
+            txt_resultado.Text += "9";
+        }
+
+        private void btn_virgula_Click(object sender, EventArgs e)
+        {
+            txt_resultado.Text += ".";
+        }
+
+        private void btn_mais_Click(object sender, EventArgs e)
+        {
+            valor1 = decimal.Parse(txt_resultado.Text, CultureInfo.InvariantCulture);
+            txt_resultado.Text = "";
+            operacao = "SOMA";
+            lbl_operacao.Text = "+";
+        }
+
+        private void btn_igual_Click(object sender, EventArgs e)
+        {
+            valor2 = decimal.Parse(txt_resultado.Text, CultureInfo.InvariantCulture);
+
+            if(operacao == "SOMA")
+            {
+                txt_resultado.Text = Convert.ToString(valor1 + valor2);
+            } 
+            else if(operacao == "SUB")
+            {
+                txt_resultado.Text = Convert.ToString(valor1 - valor2);
+            } 
+            else if(operacao == "MULT")
+            {
+                txt_resultado.Text = Convert.ToString(valor1 * valor2);
+            }
+            else
+            {
+                txt_resultado.Text = Convert.ToString(valor1 / valor2);
+            }
+        }
+
+        private void btn_menos_Click(object sender, EventArgs e)
+        {
+            valor1 = decimal.Parse(txt_resultado.Text, CultureInfo.InvariantCulture);
+            txt_resultado.Text = "";
+            operacao = "SUB";
+            lbl_operacao.Text = "-";
+        }
+
+        private void btn_vezes_Click(object sender, EventArgs e)
+        {
+            valor1 = decimal.Parse(txt_resultado.Text, CultureInfo.InvariantCulture);
+            txt_resultado.Text = "";
+            operacao = "MULT";
+            lbl_operacao.Text = "×";
+        }
+
+        private void btn_div_Click(object sender, EventArgs e)
+        {
+            valor1 = decimal.Parse(txt_resultado.Text, CultureInfo.InvariantCulture);
+            txt_resultado.Text = "";
+            operacao = "DIV";
+            lbl_operacao.Text = "÷";
+        }
+
+        private void btn_c_Click(object sender, EventArgs e)
+        {
+            txt_resultado.Text = "";
+        }
+    }
+}
